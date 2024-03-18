5e2ad1bf1c03023622ad56547fced62341fb8893
Hisam Fahri <me@hisamafahri.com>
2024-03-18 17:06:44 +0700

    fix: adjust site title

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,11 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: "Hisam's Journals",
+    description: "My thoughts, experiences, and discoveries",
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
